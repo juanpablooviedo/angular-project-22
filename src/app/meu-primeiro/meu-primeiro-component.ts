89bb5fe7cd9e63700706ab312297ba26dd14245f
juanpablooviedo <8oviedopablojuan@gmail.com>
2022-12-16 22:18:40 -0300

    primeiros componentes

---
+++ src/app/meu-primeiro/meu-primeiro-component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component ({ // informamos ao compilador que esta classe é um componente do angular
+  selector: 'meu-primeiro-component',
+  template: `<p>Meu primeiro component criado manualmente</p>`
+})
+export class MeuPrimeiroComponent {}
